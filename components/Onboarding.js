59d7286268bf2a9cac0a93baf180f9736d03d15d
codingshot <inshatk@gmail.com>
2021-01-31 22:17:13 -0500

    adding onboarding to services

---
+++ components/Onboarding.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const Onboarding = () => {
+    return (
+        <section className="pricing-one pricing-page ">
+            <div className="container">
+                <div className="block-title text-center">
+                    <h2 className="block-title__title">We recruit diverse talent, educate them, train them for your company, and onboard them <br />
+                        plan for you</h2>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <div className="pricing-one__single">
+                            <div className="pricing-one__inner">
+                                <h2 className="pricing-one__price">Recruit </h2>
+                                <p className="pricing-one__name">Across global chapters</p>
+                                <ul className="pricing-one__list list-unstyled">
+                                    <li>school students</li>
+                                    <li>university graduates</li>
+                                    <li>Upgrate options</li>
+                                    <li>9 Days Time</li>
+                                </ul>
+                                <a href="#" className="thm-btn pricing-one__btn">Choose Plan</a>
+                                <p className="pricing-one__tag-line">No hidden
+                                    charges!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="pricing-one__single">
+                            <div className="pricing-one__inner">
+                                <h2 className="pricing-one__price">Train </h2>
+                                <p className="pricing-one__name">through specialized, hands-on skills training</p>
+                                <ul className="pricing-one__list list-unstyled">
+                                    <li>3 Full Courses</li>
+                                    <li>Lifetime free support</li>
+                                    <li>Upgrate options</li>
+                                    <li>9 Days Time</li>
+                                </ul>
+                                <a href="#" className="thm-btn pricing-one__btn">Choose Plan</a>
+                                <p className="pricing-one__tag-line">No hidden
+                                    charges!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="pricing-one__single">
+                            <div className="pricing-one__inner">
+                                <h2 className="pricing-one__price">Onboard </h2>
+                                <p className="pricing-one__name">premium pack</p>
+                                <ul className="pricing-one__list list-unstyled">
+                                    <li>3 Full Courses</li>
+                                    <li>Lifetime free support</li>
+                                    <li>Upgrate options</li>
+                                    <li>9 Days Time</li>
+                                </ul>
+                                <a href="#" className="thm-btn pricing-one__btn">Choose Plan</a>
+                                <p className="pricing-one__tag-line">No hidden
+                                    charges!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Onboarding;
--- pages/services.js
+++ pages/services.js
@@ -3,14 +3,22 @@ import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
 import PageHeader from "../components/PageHeader";
 import Footer from "../components/Footer";
+import SliderTwo from "../components/SliderTwo";
+import TeamOne from "../components/TeamOne";
+import TeamTab from "../components/TeamTab";
+import Onboarding from "../components/Onboarding";
+
 import Events from "../components/Courses";
 
 const ServicesPage = () => {
     return (
         <Layout pageTitle="Services">
             <NavOne />
-            <PageHeader title="Services" />
-            <Events />
+            {/* <PageHeader title="Services" /> */}
+            <SliderTwo/>
+            <TeamOne/>
+            <TeamTab/>
+            <Onboarding/>
             <Footer />
         </Layout>
     );
