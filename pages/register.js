bffa30abb2aafa998f010d81e76a03305e000dde
pextech <mcstain1639@gmail.com>
2021-03-25 15:24:29 +0200

    finished designing the page

---
+++ pages/register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from "../components/Layout";
+import NavOne from "../components/NavOne";
+import PageHeader from "../components/PageHeader";
+import Footer from "../components/Footer";
+import RegisterSection from '../components/RegisterSection';
+
+function register() {
+    return (
+        <div>
+           <Layout pageTitle="Register">
+            <NavOne />
+            <RegisterSection />
+            <Footer />
+        </Layout>
+            
+        </div>
+    )
+}
+
+export default register;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  prefix: 'tw-',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      primary: {
+        100: '#151565',
+        200: '#1C2834',
+      },
+    },
+  },
     screens: {
       'sm': '576px',
       'md': '768px',
