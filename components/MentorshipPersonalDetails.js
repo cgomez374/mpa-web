7368bd5cfca45c546389174120d8d85356eafe96
ymmy1 <chem-pion@mail.ru>
2021-04-11 00:53:49 -0700

    styles and responsive design

--- components/MentorshipPersonalDetails.js
+++ components/MentorshipPersonalDetails.js
@@ -17,6 +17,7 @@ export class MentorshipPersonalDetails extends Component {
         this.props.values
 
         return (
+<<<<<<< HEAD
             <div className="tw-flex tw-flex-col tw-justify-between tw-font-redhat tw-bg-white tw-h-660px md:tw-h-auto tw-w-950px tw-p-24 md:tw-p-10 tw-pb-10 md:tw-my-20 tw-rounded-3xl tw-shadow-mentor">
                 <div>
                     <form>
@@ -27,10 +28,22 @@ export class MentorshipPersonalDetails extends Component {
                                 <input
                                     id="date"
                                     className="tw-outline-none tw-font-bold tw-text-black tw-text-xl"
+=======
+            <div className="tw-flex tw-flex-col tw-justify-between tw-font-redhat tw-bg-white tw-h-5/6  tw-w-950px tw-p-20 md:tw-p-10 tw-pb-5">
+                <div>
+                    <form>
+                        <h1 className="tw-font-bold tw-text-black tw-text-4xl tw-mb-4 md:tw-text-center">Personal Details</h1>
+                        <div className="tw-flex tw-flex-row md:tw-flex-col">
+                            <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col tw-text-#676565 tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-mr-2 md:tw-mr-0">
+                                <label className="tw-text-xs" htmlFor="firstName">First Name</label>
+                                <input
+                                    className="tw-outline-none"
+>>>>>>> 31f40197 (styles and responsive design)
                                     type="text"
                                     name="firstName"
                                     onChange={handleChange('firstName')}
                                     defaultValue={values.firstName}
+<<<<<<< HEAD
                                     maxLength="16"
                                 />
                             </div>
@@ -38,15 +51,27 @@ export class MentorshipPersonalDetails extends Component {
                                 <label className="tw-text-md tw-select-none tw-mb-0.5" htmlFor="lastName">Last Name</label>
                                 <input
                                     className="tw-outline-none tw-font-bold tw-text-black tw-text-xl"
+=======
+                                />
+                            </div>
+                            <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col tw-text-#676565 tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-ml-2 md:tw-ml-0">
+                                <label className="tw-text-xs" htmlFor="lastName">Last Name</label>
+                                <input
+                                    className="tw-outline-none"
+>>>>>>> 31f40197 (styles and responsive design)
                                     type="text"
                                     name="lastName"
                                     onChange={handleChange('lastName')}
                                     defaultValue={values.lastName}
+<<<<<<< HEAD
                                     maxLength="16"
+=======
+>>>>>>> 31f40197 (styles and responsive design)
                                 />
                             </div>
                         </div>
                         <div className="tw-flex tw-flex-row md:tw-flex-col">
+<<<<<<< HEAD
                             <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col  tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-mr-3 md:tw-mr-0">
                                 <label className="tw-text-md tw-select-none tw-mb-0.5" htmlFor="DOB">Date of Birth</label>
                                 <input
@@ -66,6 +91,22 @@ export class MentorshipPersonalDetails extends Component {
                                 <label className="tw-text-md tw-select-none tw-mb-0.5" htmlFor="DOBHometown">Hometown</label>
                                 <input
                                     className="tw-outline-none tw-font-bold tw-text-black tw-text-xl"
+=======
+                            <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col tw-text-#676565 tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-mr-2 md:tw-mr-0">
+                                <label className="tw-text-xs" htmlFor="DOB">Date of Birth</label>
+                                <input
+                                    className="tw-outline-none"
+                                    type="text"
+                                    name="DOB"
+                                    onChange={handleChange('DOB')}
+                                    defaultValue={values.DOB}
+                                />
+                            </div>
+                            <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col tw-text-#676565 tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-ml-2 md:tw-ml-0">
+                                <label className="tw-text-xs" htmlFor="DOBHometown">Hometown</label>
+                                <input
+                                    className="tw-outline-none"
+>>>>>>> 31f40197 (styles and responsive design)
                                     type="text"
                                     name="DOBHometown"
                                     onChange={handleChange('DOBHometown')}
@@ -74,6 +115,7 @@ export class MentorshipPersonalDetails extends Component {
                             </div>
                         </div>
                         <div className="tw-flex tw-flex-row md:tw-flex-col">
+<<<<<<< HEAD
                             <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col tw-text-textGray tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-mr-3 md:tw-mr-0">
                                 <label className="tw-text-md tw-select-none tw-mb-0.5" htmlFor="levelOfEducation">Level of Education</label>
                                 <MentorshipPersonalDetailsSelect
@@ -91,12 +133,36 @@ export class MentorshipPersonalDetails extends Component {
                                     handleChange={handleChange("passions")}
                                     handleDropDown={handleDropDown}
                                     values={values}
+=======
+                            <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col tw-text-#676565 tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-mr-2 md:tw-mr-0">
+                                <label className="tw-text-xs" htmlFor="levelOfEducation">Level of Education</label>
+                                <input
+                                    className="tw-outline-none"
+                                    type="text"
+                                    name="levelOfEducation"
+                                    onChange={handleChange('levelOfEducation')}
+                                    defaultValue={values.levelOfEducation}
+                                />
+                            </div>
+                            <div className="tw-flex tw-w-6/12 md:tw-w-full tw-flex-initial tw-flex-col tw-text-#676565 tw-bg-white input-area tw-py-3 tw-px-3 tw-border-4 tw-border-gray-300 tw-border-opacity-50 tw-rounded-2xl tw-my-2 tw-ml-2 md:tw-ml-0">
+                                <label className="tw-text-xs" htmlFor="levelOfEducationHometown">Education Hometown</label>
+                                <input
+                                    className="tw-outline-none"
+                                    type="text"
+                                    name="levelOfEducationHometown"
+                                    onChange={handleChange('levelOfEducationHometown')}
+                                    defaultValue={values.levelOfEducationHometown}
+>>>>>>> 31f40197 (styles and responsive design)
                                 />
                             </div>
                         </div>
                     </form>
                 </div>
+<<<<<<< HEAD
                 <button className="tw-bg-NavDark tw-outline-none tw-rounded-md tw-w-14 tw-mx-auto tw-mt-24 sm:tw-mt-24 tw-text-white hover:tw-text-NavDark hover:tw-bg-white tw-duration-500 tw-select-none" onClick={this.continue}><i className="fas fa-arrow-right tw-text-2xl tw-p-2"></i></button>
+=======
+                <button onClick={this.continue}>→</button>
+>>>>>>> 31f40197 (styles and responsive design)
             </div>
         )
     }
