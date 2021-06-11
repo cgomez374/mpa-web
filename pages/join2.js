2f269c24a41a4a2d7a909f765536960793597de5
pextech <mcstain1639@gmail.com>
2021-06-11 11:36:14 +0200

    updated the gitignore

--- pages/join2.js
+++ pages/join2.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
 import NavThree from "../components/NavThree";
-import "../components/join/join.css";
 import PageHeader from "../components/PageHeader";
 import Footer from "../components/Footer";
 import JoinCard from '../components/join/joinCard';
